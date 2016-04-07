dba9bf9c3bb3daaf6ed51b47f52cdc02ff5e87f3
ahmed-negm <anegm@icinetic.com>
2016-04-07 18:12:26 +0200

    clean main.swift

--- Sources/Resources/main.swift
+++ Sources/Resources/main.swift
@@ -1,25 +1,11 @@
 import Kitura
 import KituraNet
 import KituraSys
-
 import HeliumLogger
 import LoggerAPI
-
 import Foundation
 
-///
-/// The Kitura router
-///
-let router = Router()
-
-///
-/// Set up a simple Logger
-///
-Log.logger = HeliumLogger()
-
-/**
- Custom middleware that allows Cross Origin HTTP requests
- */
+// Custom middleware that allows Cross Origin HTTP requests
 class AllRemoteOriginMiddleware: RouterMiddleware {
     func handle(request: RouterRequest, response: RouterResponse, next: () -> Void) {
         response.setHeader("Access-Control-Allow-Origin", value: "*")
@@ -27,31 +13,30 @@ class AllRemoteOriginMiddleware: RouterMiddleware {
     }
 }
 
-router.all("/*", middleware: BodyParser())
+// The Kitura router
+let router = Router()
 
+// Set up a simple Logger
+Log.logger = HeliumLogger()
+
+// Apply middlewares
+router.all("/*", middleware: BodyParser())
 router.all("/*", middleware: AllRemoteOriginMiddleware())
 
-/**
- Handle options
- */
+// Handle options
 router.options("/*") {
     request, response, next in
-    
     response.setHeader("Access-Control-Allow-Headers", value: "accept, content-type")
     response.setHeader("Access-Control-Allow-Methods", value: "GET,HEAD,POST,DELETE,OPTIONS,PUT,PATCH")
-    
     response.status(HttpStatusCode.OK)
-    
     next()
 }
 
+// Setup resources
 setupChannelRoutes(router)
 
-///
-/// Listen to port 8090
-///
+// Listen to port 8090
 let port = 8090
 let server = HttpServer.listen(port, delegate: router)
 Server.run()
-
 print("Server is starting ...")
